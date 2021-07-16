e92b2be07b4dd5fc327a41a4db3878e2af35e007
이경진 <bramhome5848@gmail.com>
2021-07-16 16:17:56 +0900

    20210716 Validator 분리1

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -24,6 +24,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -217,7 +218,7 @@ public class ValidationItemControllerV2 {
      * errorArgs : 오류 메시지에서 {0} 을 치환하기 위한 값
      * defaultMessage : 오류 메시지를 찾을 수 없을 때 사용하는 기본 메시지
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         log.info("objectName={}", bindingResult.getObjectName());
@@ -239,6 +240,21 @@ public class ValidationItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        itemValidator.validate(item, bindingResult);
 
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
