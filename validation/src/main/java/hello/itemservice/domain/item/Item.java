d2df65410f7e2b879bb3d617b1aba5ea13ab8a7b
이경진 <bramhome5848@gmail.com>
2021-07-18 18:31:38 +0900

    20210716 Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,7 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -22,7 +23,14 @@ import javax.validation.constraints.NotNull;
  * @NotNull -> Null 만 허용하지 않음, "" 이나 " " 은 허용,
  * @NotEmpty -> null 과 "" 둘 다 허용하지 않음, " " 은 허용
  * @NotBlank -> null 과 "" 과 " " 모두 허용하지 않음, 3개 중 validation 강도가 높은 것
+ *
+ * 특정 필드( FieldError )가 아닌 해당 오브젝트 관련 오류( ObjectError ) 처리
+ * ->  * 다음과 같이 @ScriptAssert() 를 사용
+ * 실제 사용해보면 제약이 많고 복잡하다. 그리고 실무에서는 검증 기능이 해당 객체의 범위를 넘어서는 경우들도 종종 등장하는데,
+ * 그런 경우 대응이 어렵다. -> 오브젝트 관련 오류는 자바 코드로 직접 처리하는 것을 권장
+ *
  */
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 @Data
 public class Item {
 
