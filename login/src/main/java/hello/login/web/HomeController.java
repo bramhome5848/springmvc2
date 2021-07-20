100d2a4436c88c42c6e7bf8c0bdb03658c6ae4e6
이경진 <bramhome5848@gmail.com>
2021-07-20 16:47:03 +0900

    20210720 홈 화면

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    /**
+     * 참고
+     * 도메인 = 화면, UI, 기술 인프라 등등의 영역은 제외한 시스템이 구현해야 하는 핵심 비즈니스 업무 영역을 말함
+     * 향후 web을 다른 기술로 바꾸어도 도메인은 그대로 유지할 수 있어야 한다.
+     * 이렇게 하려면 web 은 domain 을 참조하지만 domain 은 web 을 참조하지 않아야 한다.
+     */
     @GetMapping("/")
     public String home() {
-        return "redirect:/items";
+        return "home";
     }
 }
