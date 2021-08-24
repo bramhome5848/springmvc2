00862d782a176255932a214da5a896b4d988ba8b
이경진 <bramhome5848@gmail.com>
2021-08-24 13:05:41 +0900

    20210824 스프링 타입 컨버터 소개

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,38 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    /**
+     * HTTP 요청 파라미터는 모두 문자로 처리
+     * 요청 파라미터를 자바에서 다른 타입으로 변환해서 사용하고 싶으면
+     * 숫자 타입으로 변환하는 과정을 거쳐야 함
+     */
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자 타입으로 조회
+        Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    /**
+     * 스프링이 제공하는 @RequestParam 을 사용하면 문자 10을 Integer 타입의 숫자 10으로 편리하게 받을 수 있음
+     * 이것은 스프링이 중간에서 타입을 변환해주었기 때문
+     * @ModelAttribute , @PathVariable 또한 스프링이 중간에 타입을 변환
+     *
+     * 스프링은 확장 가능한 컨버터 인터페이스를 제공
+     * 개발자는 스프링에 추가적인 타입 변환이 필요하면 이 컨버터 인터페이스를 구현해서 등록하면 됨
+     */
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
